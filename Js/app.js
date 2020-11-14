59c8935cdb901d5a93b813a8b07b927d93df4682
FelipeBellinazzi <bellinazzifelipe@gmail.com>
2020-11-14 12:33:04 -0700

    Array of Tweets

---
+++ Js/app.js
@@ -0,0 +1,39 @@
+var tweets = [
+    'A day without sunshine is like, you know, night',
+    'I always wanted to be sombody, but now I realize I should have been more specific',
+    'Age  is something that doenst matter, unless you are a cheese',
+    'Do not take life too seriously. You will never get out of it alive',
+    'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment',
+    'Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less',
+    'Sometimes life hits you in the head with a brick. Dont lose faith',
+    'We do not remember days, we remember moments',
+    'Have a very good reason for everything you do',
+    'I can take it. The tougher it gets, the cooler I get'
+
+];
+
+console.log(tweets);
+console.log(tweets[0])
+
+
+var counter = 0;
+while(counter < 10) {
+    console.log(counter);
+    counter++;
+}
+for(var counter = 0; counter < 10; counter++) {
+    console.log(counter);
+}
+
+
+for (var i = 0; i < 7; i++) {
+    console.log(tweets[i])
+}
+
+console.log('---------')
+
+
+for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i])
+}
+
